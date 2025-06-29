3bc54958007295294a3104ac79f762540d600213
Sjaiod <delwar.saj2008@gmail.com>
2025-06-29 10:45:03 +0600

    sds

--- components/nsft/GameAD.tsx
+++ components/nsft/GameAD.tsx
@@ -3,7 +3,7 @@ import React from 'react'
 const GameAD = () => {
   return (
 <section className="bg-red-300/10 py-16 flex items-center justify-center px-4 sm:px-6 lg:px-20">
-<a className='text-bold text-2xl text-blue-500' href="/games/quickdraw">Play a fun emotion game</a>
+<a className='text-bold text-2xl text-blue-500' href="/games/quickdraw">Play a fun game to win a  free event pass</a>
 
 </section>
   )
