7210dfefbdc9cac456cf1e1a8e7a1bd8f2f0f986
Sjaiod <sajid.saj2008@gmail.com>
2026-07-21 19:11:57 +0600

    Update -01 of metadata

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -6,11 +6,21 @@ import Footer from "@/components/layout/Footer";
 
 
 export const metadata: Metadata = {
-  title: "Youth's Voice",
-  description: "Youth's Voice connects young volunteers with community programs to support families, education, and health.",
+  title: "Youth's Voice - Let's create the revolution",
+  description: "A Non-profit organization working to support families, education, and health.",
   openGraph: {
-    title: "Youth's Voice",
-    description: "Youth's Voice connects young volunteers with community programs to support families, education, and health."}
+    title: "Youth's Voice - Let's create the revolution",
+    description: "A Non-profit organization working to support families, education, and health."},
+  authors:[{name: "Md. Sajid Hossain",},{name:"Youth's Voice", url: "https://youthsvoice.org/"}],
+  keywords:["youths voice", "youthsvoice", "youths voice organization", "youths voice ngo", "youths voice non-profit", "youths voice non-profit organization", "youths voice non-profit ngo", "youths voice non-profit organization ngo", "youths voice non-profit organization ngo in bangladesh", "youths voice non-profit organization ngo in bangladesh for education", "youths voice non-profit organization ngo in bangladesh for health", "youths voice non-profit organization ngo in bangladesh for families","youth volunteering organization bangladesh", "youth volunteering organization bangladesh for education", "youth volunteering organization bangladesh for health", "youth volunteering organization bangladesh for families", "youth volunteering organization bangladesh for children", "youth volunteering organization bangladesh for youth", "youth volunteering organization bangladesh for community development", "youth volunteering organization bangladesh for social development", "youth volunteering organization bangladesh for economic development", "youth volunteering organization bangladesh for environmental development","project chawnapiyaju", "fight against winter","menstrual hygiene management", "menstrual hygiene management bangladesh", "menstrual hygiene management ngo bangladesh", "menstrual hygiene management non-profit organization bangladesh", "menstrual hygiene management non-profit organization ngo bangladesh", "menstrual hygiene management" , "YV" ,"yv","ywf","YWF", "reel for red", "film festival" , "art exehibition"],
+  viewport: {
+    width: "device-width",
+    initialScale: 1,
+  },
+  icons:{
+    icon:"/logo.svg"
+  },
+  metadataBase: new URL("https://youthsvoice.org/" ),
 };
 
 export default function RootLayout({
