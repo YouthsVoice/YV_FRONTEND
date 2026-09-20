c4202660d3c7082b39cf6149ad309591b2ccea5e
YouthsVoice <youthsvoiceweb@gmail.com>
2026-09-20 20:01:51 +0600

    sd

--- src/app/donate/page.tsx
+++ src/app/donate/page.tsx
@@ -1,6 +1,9 @@
 import DonationForm from '@/components/donate/DonationForm'
 import { getVolunteerEvents } from "@/lib/api/event";
 
+export const dynamic = 'force-dynamic';
+
+
 
 export default async function DonatePage() {
   const events = await getVolunteerEvents();
