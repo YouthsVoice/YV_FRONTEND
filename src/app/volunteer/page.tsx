4aa4af9fc46c6a95e2b3da1d4c33dabcca14abc7
YouthsVoice <youthsvoiceweb@gmail.com>
2026-09-20 20:11:14 +0600

    d

--- src/app/volunteer/page.tsx
+++ src/app/volunteer/page.tsx
@@ -4,8 +4,10 @@ import VolunteerHero from "@/components/volunteer/VolunteerHero";
 import VolunteerBenefits from "@/components/volunteer/VolunteerBenefits";
 import VoluntterPage from "@/components/home/VoluntterPage";
 import { getVolunteerEvents } from "@/lib/api/event";
+export const dynamic = 'force-dynamic';
 export default async function VolunteerPage() {
   const events = await getVolunteerEvents();
+  
 
   return (
     <>
